ff7b8b7c9957a2b8fee65de07cbe118caf3eefc0
zackarias00 <zacharywarfelresume@gmail.com>
2022-05-17 21:12:59 -0500

    Successfully connected to local mysql database, Extends CrudRepository in VespersRepository is commented out to compile for now.

--- src/main/java/com/typicon/demo/data/VespersRepository.java
+++ src/main/java/com/typicon/demo/data/VespersRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VespersRepository extends CrudRepository<Vespers, Integer>{
+public interface VespersRepository /*extends CrudRepository<Vespers, Integer>*/{
 
 }
